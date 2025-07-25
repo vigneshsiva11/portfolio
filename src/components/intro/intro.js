79db4152e45d3c9808074910b97fe058f79cfda0
vigneshsiva11 <vigneshsiva076@gmail.com>
2025-07-26 00:05:37 +0530

    Fix unused imports and redundant block for Netlify build

--- src/components/intro/intro.js
+++ src/components/intro/intro.js
@@ -9,9 +9,8 @@ const Intro = () => {
               <span className='hello'>Hello</span>
               <span className='introtext'>I'am <span className="introname">Vignesh </span> <br/> website developer</span>
               <p className="intropara">I am a skilled web developer in creating visually <br /> appealing and user friendly websites</p>
-              {/* <Link><button className="btn"><img src={btnImg} alt="Hire me" className='btnImg'/>Hire me</button></Link> */}
         </div>
-        {/* <img src={bg} alt="profile" className="bg" /> */}
+        
       </section>
   )
 }
