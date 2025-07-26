9c9c6288e99626e8e48003c00e181b2a220b1276
vigneshsiva11 <vigneshsiva076@gmail.com>
2025-07-26 15:00:07 +0530

    Added resume PDF and updated Intro section

--- src/components/intro/intro.js
+++ src/components/intro/intro.js
@@ -1,6 +1,6 @@
 
 import './intro.css'
-
+import hireme from '../../assets/hireme.png'
 
 const Intro = () => {
   return (
@@ -9,8 +9,11 @@ const Intro = () => {
               <span className='hello'>Hello</span>
               <span className='introtext'>I'am <span className="introname">Vignesh </span> <br/> website developer</span>
               <p className="intropara">I am a skilled web developer in creating visually <br /> appealing and user friendly websites</p>
+              <a href="/vignesh_resume.pdf" target="_blank" rel="noopener noreferrer">
+              <button className="btn"><img src={hireme} alt="" className='btnImg'/>Resume</button>
+              </a>
+
         </div>
-        
       </section>
   )
 }
