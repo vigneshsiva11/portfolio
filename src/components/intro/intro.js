ecaec4ecfaadf1ed0cc862d136303af0e131c6e8
vigneshsiva11 <vigneshsiva076@gmail.com>
2025-09-12 15:29:25 +0530

    resume change

--- src/components/intro/intro.js
+++ src/components/intro/intro.js
@@ -9,7 +9,7 @@ const Intro = () => {
               <span className='hello'>Hello</span>
               <span className='introtext'>I'am <span className="introname">Vignesh </span> <br/> website developer</span>
               <p className="intropara">I am a skilled web developer in creating visually <br /> appealing and user friendly websites</p>
-              <a href="/vignesh_resume.pdf" target="_blank" rel="noopener noreferrer">
+              <a href="/vignesh-resume.pdf" target="_blank" rel="noopener noreferrer">
               <button className="btn"><img src={hireme} alt="" className='btnImg'/>Resume</button>
               </a>
         </div>
